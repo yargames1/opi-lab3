b6db0ed830fed4bd67a3dfa8b5afd020087b41b9
TheGeniusOfEternity <max06safin@yandex.ru>
2026-05-10 21:25:06 +0300

    feat(tests): clock push bean

---
+++ src/test/java/org/example/tests/ClockPushBeanTest.java
@@ -0,0 +1,25 @@
+package org.example.tests;
+
+import org.example.beans.ClockPushBean;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+public class ClockPushBeanTest {
+
+    private final ClockPushBean bean = new ClockPushBean();
+
+    @Test
+    public void setUp() {
+        try {
+            bean.init();
+            assertNotNull("Scheduler должен быть запущен", bean);
+            bean.destroy();
+        } catch (Exception e) {
+            fail("Init не должен падать без PushContext: " + e.getMessage());
+        }
+    }
+
+
+}
